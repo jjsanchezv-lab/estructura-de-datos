9676ff92fb5dd91be160512f2cbc45631e706b94
jjsanchezv-lab <jjsanchezv@ufpso.edu.co>
2026-02-25 20:11:47 -0600

    Mini proyecto

---
+++ mi-proyecto/script.js
@@ -0,0 +1,12 @@
+const toggleButton = document.getElementById("themeToggle");
+const body = document.body;
+
+toggleButton.addEventListener("click", () => {
+    body.classList.toggle("dark-mode");
+
+    if (body.classList.contains("dark-mode")) {
+        toggleButton.textContent = "☀";
+    } else {
+        toggleButton.textContent = "🌙";
+    }
+});
