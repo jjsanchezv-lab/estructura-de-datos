afc698c6664efc70fc7d441144e921c6ba48a534
jjsanchezv-lab <jjsanchezv@ufpso.edu.co>
2026-05-26 17:25:53 -0500

    Borrando lo del parcial 1

--- sagrada/js/components.js
+++
@@ -1,9 +0,0 @@
-async function loadComponent(id, file){
-    const res = await fetch(file);
-    const html = await res.text();
-    document.getElementById(id).innerHTML = html;
-}
-
-loadComponent("header", "components/header.html");
-loadComponent("footer", "components/footer.html");
-loadComponent("sidebar", "components/sidebar.html");
